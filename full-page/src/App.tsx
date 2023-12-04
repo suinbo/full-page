97e58cadcd5ce532364981f4059bc38ce0cb3f21
suinbo <suin9610@gmail.com>
2023-12-04 13:03:40 +0900

    [Feat] full-page 화면 스크롤 적용
    1. App.scss
    - body 내 스크롤 방지
    - container 에서 스크롤 제어
    
    2. lint 설정 추가
    - 사용하지 않는 변수 경고

--- full-page/src/App.tsx
+++ full-page/src/App.tsx
@@ -1,8 +1,28 @@
+import React, { useEffect, useRef } from "react"
 import "./App.scss"
 
 const App = () => {
+    const containerRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        const wheelHandler = (e: WheelEvent) => {
+            //e.preventDefault()
+        }
+
+        const containerRefCurrent = containerRef.current
+
+        if (containerRefCurrent) {
+            containerRefCurrent.addEventListener("wheel", wheelHandler)
+        }
+
+        // 컴포넌트 언마운트 시 wheelHandler 제거
+        return () => {
+            containerRefCurrent?.removeEventListener("wheel", wheelHandler)
+        }
+    }, [])
+
     return (
-        <div className="container">
+        <div className="container" ref={containerRef}>
             <div className="section bg1"></div>
             <div className="section bg2"></div>
             <div className="section bg3"></div>
