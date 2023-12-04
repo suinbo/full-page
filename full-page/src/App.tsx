ac5902eb7e3b1da41a5f219b1385ce2c71329a15
suinbo <suin9610@gmail.com>
2023-12-04 14:01:52 +0900

    [Feat] 스크롤 시 페이지 이동 기능 구현

--- full-page/src/App.tsx
+++ full-page/src/App.tsx
@@ -1,23 +1,78 @@
-import React, { useEffect, useRef } from "react"
+import { useEffect, useRef } from "react"
 import "./App.scss"
 
 const App = () => {
     const containerRef = useRef<HTMLDivElement>(null)
 
     useEffect(() => {
+        const divRef = containerRef.current
+
         const wheelHandler = (e: WheelEvent) => {
-            //e.preventDefault()
+            e.preventDefault()
+
+            const { deltaY } = e // 음수: 스크롤 올릴 때, 양수: 스크롤 내릴 때
+            const pageHeight = window.innerHeight // 화면 세로길이(100vh)
+
+            if (divRef) {
+                const scrollTop = divRef.scrollTop // 스크롤 최상단 위치
+
+                if (deltaY > 0) {
+                    if (scrollTop >= 0 && scrollTop < pageHeight) {
+                        // 1페이지
+                        divRef.scrollTo({
+                            top: pageHeight,
+                            left: 0,
+                            behavior: "smooth",
+                        })
+                    } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+                        // 2페이지
+                        divRef.scrollTo({
+                            top: pageHeight * 2,
+                            left: 0,
+                            behavior: "smooth",
+                        })
+                    } else {
+                        // 3페이지
+                        divRef.scrollTo({
+                            top: pageHeight * 2,
+                            left: 0,
+                            behavior: "smooth",
+                        })
+                    }
+                } else {
+                    if (scrollTop >= 0 && scrollTop < pageHeight) {
+                        // 1페이지
+                        divRef.scrollTo({
+                            top: 0,
+                            left: 0,
+                            behavior: "smooth",
+                        })
+                    } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+                        // 2페이지
+                        divRef.scrollTo({
+                            top: 0,
+                            left: 0,
+                            behavior: "smooth",
+                        })
+                    } else {
+                        // 3페이지
+                        divRef.scrollTo({
+                            top: pageHeight,
+                            left: 0,
+                            behavior: "smooth",
+                        })
+                    }
+                }
+            }
         }
 
-        const containerRefCurrent = containerRef.current
-
-        if (containerRefCurrent) {
-            containerRefCurrent.addEventListener("wheel", wheelHandler)
+        if (divRef) {
+            divRef.addEventListener("wheel", wheelHandler)
         }
 
-        // 컴포넌트 언마운트 시 wheelHandler 제거
+        // 컴포넌트 언마운트 시 이벤트 제거 (메모리 누수 방지)
         return () => {
-            containerRefCurrent?.removeEventListener("wheel", wheelHandler)
+            divRef?.removeEventListener("wheel", wheelHandler)
         }
     }, [])
 
