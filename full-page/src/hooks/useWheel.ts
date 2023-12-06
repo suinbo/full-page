d481cc5470ac85a219dd4288f62e75ccd1fa1460
suinbo <suin9610@gmail.com>
2023-12-06 15:48:24 +0900

    [Refactor] useWheel 훅 성능 개선
    1. Debounce 기법 적용
    -  이벤트가 연속적으로 발생할 때 함수가 동시에 여러번 호출되는 것을 막고, 일정 시간이 지나기 전 마지막 이벤트 만을 처리
    
    [이슈]
    3 페이지에서 한 틱 이상 스크롤 업 했을 때 2 페이지가 아닌 1 페이지로 이동 (한 틱만 스크롤 업 했을 때는 2페이지로 이동)
    
    -> 마우스 휠 한 틱씩 스크롤 할때마다 1회의 이벤트가 발생하는데, 스크롤 업 할때 한 틱 이상 스크롤 업 되면서 여러 이벤트 발생
    -> handleMouseWheel - callback 은 0.5초 동안 한 번만 실행되도록 함. (3 페이지 > 2 페이지로 이동)

--- full-page/src/hooks/useWheel.ts
+++ full-page/src/hooks/useWheel.ts
@@ -4,14 +4,22 @@ type CallbackType = (ref: React.RefObject<HTMLDivElement>, deltaY: number, scrol
 
 /**
  * 마우스 휠을 감지하여 콜백함수를 처리할 수 있는 ref 객체를 반환하는 커스텀 훅.
+ * @description Debounce 기법 적용하여 스크롤 시 많은 이벤트 발생해도 한 번만 처리.
  * @param callback (deltaY: 수직 방향 스크롤 움직임 크기(px) , scrollTop: 스크롤 최상단 위치)
  */
 const useWheel = (callback: CallbackType): React.RefObject<HTMLDivElement> => {
     const ref = useRef<HTMLDivElement>(null)
+    const timeoutRef = useRef<number | null>(null)
 
     const handleMouseWheel = (e: WheelEvent) => {
         e.preventDefault()
-        callback(ref, e.deltaY, ref.current?.scrollTop!)
+
+        if (!timeoutRef.current) {
+            callback(ref, e.deltaY, ref.current?.scrollTop!)
+            timeoutRef.current = window.setTimeout(() => {
+                timeoutRef.current = null
+            }, 500)
+        }
     }
 
     useEffect(() => {
@@ -25,6 +33,10 @@ const useWheel = (callback: CallbackType): React.RefObject<HTMLDivElement> => {
             // 컴포넌트 언마운트 시 이벤트 핸들러 제거 (메모리 누수 방지)
             if (currentRef) {
                 currentRef.removeEventListener("wheel", handleMouseWheel)
+            }
+
+            if (timeoutRef.current) {
+                window.clearTimeout(timeoutRef.current)
             }
         }
     }, [ref, callback])
