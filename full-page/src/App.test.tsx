39f55c5588127403e373f89dc0f2b3dbdf22c7e2
suinbo <suin9610@gmail.com>
2023-12-07 14:33:02 +0900

    [Chore] craco 와 호환되지 않는 테스트 파일 제거
    - CRA 프로젝트는 'react-scripts' 를 사용하여 테스트 러너와 설정이 그에 맞게 구성되어 있음. craco 사용시 테스트 관련 설정은 craco.config.ts 에서 해야함.

--- full-page/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react"
-import App from "./App"
-
-test("renders full-page", () => {
-    render(<App />)
-    const linkElement = screen.getByText(/full-page/i)
-    expect(linkElement).toBeInTheDocument()
-})
